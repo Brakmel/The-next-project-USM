2f9d89b09b9f0c18c5216bf05f1e234efb1ea1d4
BMHV <94775980+Brakmel@users.noreply.github.com>
2022-09-04 23:59:37 -0300

    Add files via upload

---
+++ SalaHibrida-0.1/javascript/panel.js
@@ -0,0 +1,63 @@
+/*Panel 1*/
+function A101N(){
+    document.getElementById('A101').style.display = 'none';
+}
+function A101B(){
+    document.getElementById('A101').style.display = 'block';
+}
+/*Panel 2*/
+function A112N(){
+    document.getElementById('A112').style.display = 'none';
+}
+function A112B(){
+    document.getElementById('A112').style.display = 'block';
+}
+/*Panel 3*/
+function BELLOTON(){
+    document.getElementById('BELLOTO').style.display = 'none';
+}
+function BELLOTOB(){
+    document.getElementById('BELLOTO').style.display = 'block';
+}
+/*Panel 4*/
+function B102N(){
+    document.getElementById('B102').style.display = 'none';
+}
+function B102B(){
+    document.getElementById('B102').style.display = 'block';
+}
+/*Panel 5*/
+function B103N(){
+    document.getElementById('B103').style.display = 'none';
+}
+function B103B(){
+    document.getElementById('B103').style.display = 'block';
+}
+/*Panel 6*/
+function B105N(){
+    document.getElementById('B105').style.display = 'none';
+}
+function B105B(){
+    document.getElementById('B105').style.display = 'block';
+}
+/*Panel 7*/
+function B201N(){
+    document.getElementById('B201').style.display = 'none';
+}
+function B201B(){
+    document.getElementById('B201').style.display = 'block';
+}
+/*Panel 8*/
+function B202N(){
+    document.getElementById('B202').style.display = 'none';
+}
+function B202B(){
+    document.getElementById('B202').style.display = 'block';
+}
+/*Panel 9*/
+function B203N(){
+    document.getElementById('B203').style.display = 'none';
+}
+function B203B(){
+    document.getElementById('B203').style.display = 'block';
+}
