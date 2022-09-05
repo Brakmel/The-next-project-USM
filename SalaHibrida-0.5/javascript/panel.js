5b8ccd04226d750618e82fffe58005cddf55639d
BMHV <94775980+Brakmel@users.noreply.github.com>
2022-09-05 18:56:12 -0300

    Add files via upload

---
+++ SalaHibrida-0.5/javascript/panel.js
@@ -0,0 +1,147 @@
+/*Esto es de "panel.html"*/
+/*Panel 1*/
+function A101N(){
+    document.getElementById('A101').style.display = 'none';
+}
+function A101B(){
+    document.getElementById('A101').style.display = 'flex';
+}
+/*Panel 2*/
+function A112N(){
+    document.getElementById('A112').style.display = 'none';
+}
+function A112B(){
+    document.getElementById('A112').style.display = 'flex';
+}
+/*Panel 3*/
+function BELLOTON(){
+    document.getElementById('BELLOTO').style.display = 'none';
+}
+function BELLOTOB(){
+    document.getElementById('BELLOTO').style.display = 'flex';
+}
+/*Panel 4*/
+function B102N(){
+    document.getElementById('B102').style.display = 'none';
+}
+function B102B(){
+    document.getElementById('B102').style.display = 'flex';
+}
+/*Panel 5*/
+function B103N(){
+    document.getElementById('B103').style.display = 'none';
+}
+function B103B(){
+    document.getElementById('B103').style.display = 'flex';
+}
+/*Panel 6*/
+function B105N(){
+    document.getElementById('B105').style.display = 'none';
+}
+function B105B(){
+    document.getElementById('B105').style.display = 'flex';
+}
+/*Panel 7*/
+function B201N(){
+    document.getElementById('B201').style.display = 'none';
+}
+function B201B(){
+    document.getElementById('B201').style.display = 'flex';
+}
+/*Panel 8*/
+function B202N(){
+    document.getElementById('B202').style.display = 'none';
+}
+function B202B(){
+    document.getElementById('B202').style.display = 'flex';
+}
+/*Panel 9*/
+function B203N(){
+    document.getElementById('B203').style.display = 'none';
+}
+function B203B(){
+    document.getElementById('B203').style.display = 'flex';
+}
+/*Esto es para el login*/
+function login(){
+    var user, pass;
+
+    user = document.getElementById("usua").value;
+    pass = document.getElementById("contra").value;
+
+    if(user == "USM" && pass == "CV2022soP"){
+        window.location= "http://127.0.0.1:5500/htmls/panel.html"
+    }
+}
+/*Esto es de "tutorial.html"*/
+/*Palen de tutorial 1*/
+function paso1N(){
+    document.getElementById('main_1').style.display = 'none';
+}
+function paso1B(){
+    document.getElementById('main_1').style.display = 'block';
+}
+/*Palen de tutorial 2*/
+function paso2N(){
+    document.getElementById('main_2').style.display = 'none';
+}
+function paso2B(){
+    document.getElementById('main_2').style.display = 'block';
+}
+/*Palen de tutorial 3*/
+function paso3N(){
+    document.getElementById('main_3').style.display = 'none';
+}
+function paso3B(){
+    document.getElementById('main_3').style.display = 'block';
+}
+/*Palen de tutorial 4*/
+function paso4N(){
+    document.getElementById('main_4').style.display = 'none';
+}
+function paso4B(){
+    document.getElementById('main_4').style.display = 'block';
+}
+/*Palen de tutorial 5*/
+function paso5N(){
+    document.getElementById('main_5').style.display = 'none';
+}
+function paso5B(){
+    document.getElementById('main_5').style.display = 'block';
+}
+/*Palen de tutorial 6*/
+function paso6N(){
+    document.getElementById('main_6').style.display = 'none';
+}
+function paso6B(){
+    document.getElementById('main_6').style.display = 'block';
+}
+/*Palen de tutorial 7*/
+function paso7N(){
+    document.getElementById('main_7').style.display = 'none';
+}
+function paso7B(){
+    document.getElementById('main_7').style.display = 'block';
+}
+/*Palen de tutorial 8*/
+function paso8N(){
+    document.getElementById('main_8').style.display = 'none';
+}
+function paso8B(){
+    document.getElementById('main_8').style.display = 'block';
+}
+/*Palen de tutorial 9*/
+function paso9N(){
+    document.getElementById('main_9').style.display = 'none';
+}
+function paso9B(){
+    document.getElementById('main_9').style.display = 'block';
+}
+/*Palen de tutorial 10*/
+function paso10N(){
+    document.getElementById('main_10').style.display = 'none';
+}
+function paso10B(){
+    document.getElementById('main_10').style.display = 'block';
+}
+
